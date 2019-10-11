d0de86bc45a023c5ff360bae8d3d10b407e24316
miguelsanchezp <miguelsanchez1703@gmail.com>
2019-10-12 00:35:35 +0200

    Initialized the different components on the main activity

--- app/src/main/java/com/miguelsanchezp/hackupcproject/MainActivity.java
+++ app/src/main/java/com/miguelsanchezp/hackupcproject/MainActivity.java
@@ -11,6 +11,8 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,13 +22,44 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Toast.makeText(MainActivity.this, "settings dialog should display here", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton PendingButton = findViewById(R.id.PendingButton);
+        FloatingActionButton PlottingButton = findViewById(R.id.PlottingButton);
+        TextView DailyScore = findViewById(R.id.DailyScore);
+        TextView WeeklyScore = findViewById (R.id.WeeklyScore);
+        TextView MonthlyScore = findViewById(R.id.MonthlyScore);
+        TextView PercentageEco = findViewById (R.id.PercentageEco);
+        TextView DistanceEco = findViewById (R.id.DistanceEco);
+        TextView TimeEco = findViewById (R.id.TimeEco);
+        TextView PercentagePrivate = findViewById(R.id.PercentagePrivate);
+        TextView DistancePrivate = findViewById(R.id.DistancePrivate);
+        TextView TimePrivate = findViewById (R.id.TimePrivate);
+        TextView PercentagePublic = findViewById(R.id.PercentagePublic);
+        TextView DistancePublic = findViewById (R.id.DistancePublic);
+        TextView TimePublic = findViewById (R.id.TimePublic);
+        TextView ValEmission = findViewById(R.id.ValEmission);
+        TextView ValAvoided = findViewById(R.id.ValAvoided);
+
+        PendingButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Pending Dialog must be here", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                Toast.makeText(MainActivity.this, "Pending dialog should display here", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        PlottingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Plotting dialog should display here", Toast.LENGTH_SHORT).show();
+//                Snackbar.make(view, "Plotting DIalog must be here", Snackbar.LENGTH_LONG).setAction("Action", null).show();
             }
         });
     }
