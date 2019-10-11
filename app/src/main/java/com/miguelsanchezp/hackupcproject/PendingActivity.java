89ea3cf3f07429f6d2bca3343dfacc0bdfec9c4f
miguelsanchezp <miguelsanchez1703@gmail.com>
2019-10-12 01:04:30 +0200

    Created the PendingActivity.java class to handle the layout

---
+++ app/src/main/java/com/miguelsanchezp/hackupcproject/PendingActivity.java
@@ -0,0 +1,14 @@
+package com.miguelsanchezp.hackupcproject;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PendingActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pending_activity);
+    }
+}
