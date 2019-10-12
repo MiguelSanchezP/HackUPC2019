721964c853b5327bcd931738e20451693b585638
miguelsanchezp <miguelsanchez1703@gmail.com>
2019-10-12 06:49:38 +0200

    made the class write data be able to write on files

--- app/src/main/java/com/miguelsanchezp/hackupcproject/WriteData.java
+++ app/src/main/java/com/miguelsanchezp/hackupcproject/WriteData.java
@@ -1,4 +1,31 @@
 package com.miguelsanchezp.hackupcproject;
 
+import android.content.Context;
+import android.widget.Toast;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
 public class WriteData {
+    public static void WriteIntoFile (Data data) {
+        try{
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter (MainActivity.getContext().openFileOutput("data.txt", Context.MODE_PRIVATE));
+            outputStreamWriter.write(data.getEcoFriendlySecs());
+            outputStreamWriter.write("&");
+            outputStreamWriter.write(data.getPrivateSecs());
+            outputStreamWriter.write("&");
+            outputStreamWriter.write(data.getPublicSecs());
+            outputStreamWriter.write("&");
+            outputStreamWriter.write(String.valueOf(data.getEcoFriendlyDistance()));
+            outputStreamWriter.write("&");
+            outputStreamWriter.write(String.valueOf(data.getPrivateDistance()));
+            outputStreamWriter.write("&");
+            outputStreamWriter.write(String.valueOf(data.getPublicDistance()));
+
+        }catch (FileNotFoundException e) {
+            Toast.makeText(MainActivity.getContext(), "File wasn't found", Toast.LENGTH_LONG).show();
+        }catch (IOException e) {
+            Toast.makeText(MainActivity.getContext(), "IOException", Toast.LENGTH_LONG).show();
+        }
+    }
 }
