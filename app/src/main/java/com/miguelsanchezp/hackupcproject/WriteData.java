dd14993cbf8fc8d8dc1333e607c25f1afded5509
miguelsanchezp <miguelsanchez1703@gmail.com>
2019-10-12 16:05:51 +0200

    made some modifications and corrections on the path

--- app/src/main/java/com/miguelsanchezp/hackupcproject/WriteData.java
+++ app/src/main/java/com/miguelsanchezp/hackupcproject/WriteData.java
@@ -46,19 +46,42 @@ class WriteData {
         }
     }
 
-    static void exportScore () {
+    static void exportScore (double score) {
         File file = new File ("/data/data/com.miguelsanchezp.hackupcproject/files/scores.txt");
+        if (file.exists()) {
+            File file1 = new File("/data/data/com.miguelsanchezp.hackupcproject/files/", "scores.txt");
+            String prev = ReadData.getVal ("/data/data/com.miguelsanchezp.hackupcproject/files/", "scores.txt");
+            try {
+                FileOutputStream fileOutputStream = new FileOutputStream(file1);
+                if (prev!=null) {
+                    fileOutputStream.write(prev.getBytes());
+                }
+                fileOutputStream.write(String.valueOf(score).getBytes());
+                fileOutputStream.write("-".getBytes());
+                fileOutputStream.close();
+            }catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else{
+            try {
+                file.createNewFile();
+            }catch(IOException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 
     static void exportMovement (double mov) {
         File file = new File ("/data/data/com.miguelsanchezp.hackupcproject/files/movement.txt");
         if (file.exists()) {
-            String Movement = ReadData.getVal("/data/data/com.miguelsanchezp.hackupcproject/files/movement.txt");
+            String Movement = ReadData.getVal("/data/data/com.miguelsanchezp.hackupcproject/files/", "movement.txt");
             if (Movement != null) {
                 double movement = Double.parseDouble(Movement);
                 movement+=mov;
                 try {
-                    FileOutputStream fileOutputStream = new FileOutputStream(file);
+                    File file1 = new File ("/data/data/com.miguelsanchezp.hackupcproject/files/", "movement.txt");
+                    FileOutputStream fileOutputStream = new FileOutputStream(file1);
                     fileOutputStream.write(String.valueOf(movement).getBytes());
                     fileOutputStream.close();
                 }catch (FileNotFoundException e) {
@@ -69,6 +92,7 @@ class WriteData {
             }
         }else {
             try {
+                File file1 = new File ("/data/data/com.miguelsanchezp.hackupc.project/files/", "movement.txt");
                 file.createNewFile();
                 FileOutputStream fileOutputStream = new FileOutputStream(file);
                 fileOutputStream.write(String.valueOf(0).getBytes());
