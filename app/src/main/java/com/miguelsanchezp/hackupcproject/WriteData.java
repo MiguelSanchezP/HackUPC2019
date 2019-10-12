37c6beb1e306fe4575a9dd6e110f34ffc22c4db0
miguelsanchezp <miguelsanchez1703@gmail.com>
2019-10-12 14:50:20 +0200

    Added the creation of the methods

--- app/src/main/java/com/miguelsanchezp/hackupcproject/WriteData.java
+++ app/src/main/java/com/miguelsanchezp/hackupcproject/WriteData.java
@@ -1,28 +1,21 @@
 package com.miguelsanchezp.hackupcproject;
 
-import android.content.Context;
 import android.util.Log;
 import android.widget.Toast;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 
-public class WriteData {
+class WriteData {
     private static final String TAG = "WriteData";
 
-    public static void WriteIntoFile (Data data) {
+    static void WriteIntoFile (Data data) {
         File file = new File(  "/data/data/com.miguelsanchezp.hackupcproject/files/data.txt");
         if (file.exists()) {
-            Log.d(TAG, "WriteIntoFile: File exists");
             try {
                 File fileDef = new File ("/data/data/com.miguelsanchezp.hackupcproject/files/", "data.txt");
                 FileOutputStream fileOutputStream = new FileOutputStream(fileDef);
-//                OutputStreamWriter outputStreamWriter = new OutputStreamWriter("/data/data/com.miguelsanchezp.hackupcproject/files/data.txt", Context.MODE_PRIVATE);
-//                OutputStreamWriter outputStreamWriter = new OutputStreamWriter()
                 fileOutputStream.write(String.valueOf(data.getEcoFriendlySecs()).getBytes());
                 fileOutputStream.write("&".getBytes());
                 fileOutputStream.write(String.valueOf(data.getPrivateSecs()).getBytes());
@@ -41,17 +34,48 @@ public class WriteData {
                 Toast.makeText(MainActivity.getContext(), "IOException", Toast.LENGTH_LONG).show();
             }
         }else{
-            Log.d(TAG, "WriteIntoFile: File doesn't exist");
             File file1 = new File ("/data/data/com.miguelsanchezp.hackupcproject/files/", "data.txt");
             try {
-                Log.d(TAG, "WriteIntoFile: Creating File");
                 file1.createNewFile();
+                FileOutputStream fileOutputStream = new FileOutputStream(file1);
+                fileOutputStream.write("0&0&0&0&0&0".getBytes());
+                fileOutputStream.close();
             }catch (IOException e) {
-                Log.d(TAG, "WriteIntoFile: Error creating File");
                 e.printStackTrace();
-//                Toast.makeText(MainActivity.getContext(), "Impossible to create a file", Toast.LENGTH_LONG).show();
             }
-//            Toast.makeText(MainActivity.getContext(), "File has to be created", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    static void exportScore () {
+        File file = new File ("/data/data/com.miguelsanchezp.hackupcproject/files/scores.txt");
+    }
+
+    static void exportMovement (double mov) {
+        File file = new File ("/data/data/com.miguelsanchezp.hackupcproject/files/movement.txt");
+        if (file.exists()) {
+            String Movement = ReadData.getVal("/data/data/com.miguelsanchezp.hackupcproject/files/movement.txt");
+            if (Movement != null) {
+                double movement = Double.parseDouble(Movement);
+                movement+=mov;
+                try {
+                    FileOutputStream fileOutputStream = new FileOutputStream(file);
+                    fileOutputStream.write(String.valueOf(movement).getBytes());
+                    fileOutputStream.close();
+                }catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }else {
+            try {
+                file.createNewFile();
+                FileOutputStream fileOutputStream = new FileOutputStream(file);
+                fileOutputStream.write(String.valueOf(0).getBytes());
+                fileOutputStream.close();
+            }catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
