df679a42fc58db1a15a0498ff0b88f719a0f5ff4
miguelsanchezp <miguelsanchez1703@gmail.com>
2019-10-13 00:23:36 +0200

    made the pendingitem object to operate with it

--- app/src/main/java/com/miguelsanchezp/hackupcproject/PendingItem.java
+++ app/src/main/java/com/miguelsanchezp/hackupcproject/PendingItem.java
@@ -1,4 +1,28 @@
 package com.miguelsanchezp.hackupcproject;
 
 public class PendingItem {
+    private String initialTime;
+    private String finalTime;
+    private String distance;
+
+    public PendingItem (String initialTime, String finalTime, String distance) {
+        this.initialTime = initialTime;
+        this.finalTime = finalTime;
+        this.distance = distance;
+    }
+
+    public PendingItem (String initialTime, String finalTime) {
+        this.initialTime = initialTime;
+        this.finalTime = finalTime;
+    }
+
+    public String getInitialTime () {
+        return initialTime;
+    }
+    public String getFinalTime () {
+        return finalTime;
+    }
+    public String getDistance () {
+        return distance;
+    }
 }
